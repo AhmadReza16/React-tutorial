0e56d06fad2e0704d4c2d06f6fc29856cd3804be
AhmadReza16git config  user,email <101175583+AhmadReza16@users.noreply.github.com>
2025-08-01 18:12:05 +0330

    React-tutorial

---
+++ videos 1-10/7-Click Event/src/Button.jsx
@@ -0,0 +1,8 @@
+
+function Button(){
+
+    const handleClick = (e) => e.target.textContent = "OUCH! 🤕";
+
+    return(<button onClick={(e) => handleClick(e)}>Click me 😀</button>);
+}
+export default Button
